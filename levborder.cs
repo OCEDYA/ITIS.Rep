c6b2b69136df7b4e7b3f1332f1610541d8fcf72d
OCEDYA <boolred26@gmail.com>
2025-12-01 14:06:06 +0500

    Create levborder.cs

---
+++ levborder.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace Autocomplete;
+
+public class LeftBorderTask
+{
+    public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+    {
+        if (right - left <= 1)
+            return left;
+        
+        int mid = left + (right - left) / 2;
+        
+        if (string.Compare(phrases[mid], prefix, StringComparison.InvariantCultureIgnoreCase) < 0 
+            && !phrases[mid].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
+            return GetLeftBorderIndex(phrases, prefix, mid, right);
+        else
+            return GetLeftBorderIndex(phrases, prefix, left, mid);
+    }
